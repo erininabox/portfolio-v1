d6d08cc4fb63140605d4f10e4bcb5ebff5a53653
erininabox <erininabox@gmail.com>
2022-01-27 16:44:05 -0800

    removed stuff that looked like links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,23 +7,8 @@ class Home extends Component {
                 <h2 className="emphasis-text">Hi! I'm Erin,</h2> 
                 <p>I am a resourceful and analytical <span className="emphasis-text">full-stack software developer</span> from a non-traditional background who believes in using clean, maintainable, and well-commented code to create ethical products centered around the user experience. With over a decade of experience in education and community mental health, I believe in prioritizing healthy and productive team dynamics through transparency and communication. I bring to the table a <span className="emphasis-text">passion for accessibility</span> and a demonstrated talent at <span className="emphasis-text">cultivating exceptional teams</span> focused on problem-solving using best practices. 
                 </p>
-                <h2 className="emphasis-text">Highlighted Skills</h2>
-                <div className="highlighted-skills">
-                    <span className="skill-hp">React</span>
-                    <span className="skill-hp">JavaScript</span>
-                    <span className="skill-hp">HTML </span>
-                    <span className="skill-hp">CSS </span>
-                    <span className="skill-hp">Flexbox</span>
-                    <span className="skill-hp">Grid</span>
-                    <span className="skill-hp">WordPress</span>
-                    <span className="skill-hp">MongoDB</span>
-                    <span className="skill-hp">Express.js</span>
-                    <span className="skill-hp">SQL</span>
-                    <span className="skill-hp">PostgreSQL</span>
-                    <span className="skill-hp">Linux</span>
-                    <span className="skill-hp">Crisis management</span>
-                    <span className="skill-hp">Team building</span>
-                </div>
+
+                
             </div>
         
         )
